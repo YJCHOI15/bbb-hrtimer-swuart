23f3349921fa96051d9027a57d8cbf22066b19a4
YJCHOI15 <ccyjin15@gmail.com>
2025-08-26 19:53:33 +0900

    delete bbb-headers

--- bbb-headers/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1bookworm */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#1bookworm SMP PREEMPT Thu Aug 7 20:26:05 UTC 2025"
-#define LINUX_COMPILE_BY "voodoo"
-#define LINUX_COMPILE_HOST "rpi4b4g-13"
-#define LINUX_COMPILER "gcc (Debian 12.2.0-14+deb12u1) 12.2.0, GNU ld (GNU Binutils for Debian) 2.40"
